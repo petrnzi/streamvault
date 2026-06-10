9cda08ec23ca41c4e2ae215ba99daab947d9da55
Peter Renzi <255759548+petrnzi@users.noreply.github.com>
2026-06-10 23:44:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // Replace 'streamvault' with your exact GitHub repo name
-  base: "/streamvault/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   server: {
     port: 5173,
